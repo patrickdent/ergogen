ca8cf51c3eb3f73cec929969ba56f017257100b3
by80 <by80@M-F210JDW2DT.nordstrom.net>
2024-05-10 09:05:33 -0700

    start working on adjustable nicenano pretty footprint

--- src/footprints/index.js
+++ src/footprints/index.js
@@ -7,6 +7,7 @@ module.exports = {
     jstph: require('./jstph'),
     jumper: require('./jumper'),
     mx: require('./mx'),
+    nicenano_pretty: require('./nicenano_pretty'),
     oled: require('./oled'),
     omron: require('./omron'),
     pad: require('./pad'),

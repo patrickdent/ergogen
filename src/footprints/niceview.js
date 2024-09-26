02f5c358e7ff53452489f08470c5736aab3a8a96
patrick dent <patrick.dent@nordstrom.com>
2024-09-26 16:12:18 -0700

    niceview is a little messy still but may work

--- src/footprints/niceview.js
+++ src/footprints/niceview.js
@@ -53,6 +53,14 @@ module.exports = {
                 net: p.CS_from,
             },
         }
+
+        const via_sep = 2
+        const gen_via = (from_x, from_y, num, net) => {
+            return `
+              (pad ${num} thru_hole circle (at ${from_x + via_sep} ${from_y}) (size 0.8 0.8) (drill 0.4) (layers *.Cu *.Mask) ${net})
+            `
+          }
+      
         const jumper_sep = 0.725
         const jumper_from_x = 2
         const jumper_to_x = jumper_from_x + jumper_sep
@@ -79,12 +87,19 @@ module.exports = {
             `
         }
 
-        const gen_pin = (front_net, back_net, position) => {
-            return `
+        const gen_pin = (front_net, back_net, position, omit_jumpers=false) => {
+            let base = `
                 (pad ${nets[front_net].pin} thru_hole oval (at 0.0 ${position} ${p.rot}) (size 1.7 1.7) (drill 1) (layers *.Cu *.Mask)
                 ${nets[front_net].net.str})
+            `
+            if (omit_jumpers) {
+                return base
+            }
+            return `
+                ${base} 
                 ${gen_jumper_pad(position, p.rot, front_net, front_net, "F")}
                 ${gen_jumper_pad(position, p.rot, front_net, back_net, "B")}
+                ${gen_via(jumper_from_x, position, nets[front_net].pin, nets[front_net].nn_net.str)}
             `
         }
         const standard = `
@@ -98,7 +113,7 @@ module.exports = {
             ${'' /* pins */}
             ${gen_pin(SDA, CS, -5.08)}
             ${gen_pin(SCL, GND, -2.54)}
-            ${gen_pin(VCC, VCC, 0)}
+            ${gen_pin(VCC, VCC, 0, true)}
             ${gen_pin(GND, SCL, 2.54)}
             ${gen_pin(CS, SDA, 5.08)}
 
